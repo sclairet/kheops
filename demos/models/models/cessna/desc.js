4ba4439341b512a68c5ccfb0dd1da24d4d751fd3
sclairet <stephane.clairet@wakanda.io>
2017-05-17 18:20:29 +0200

    materials and colors mapping support

--- demos/models/models/cessna/desc.js
+++ demos/models/models/cessna/desc.js
@@ -8,6 +8,50 @@ kh.models.desc.cessna = {
 	'name': 'cessna',
 	'src': './models/cessna/cessna.obj',
 	'materials': {
+		'yellow': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+        'red': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+        'black': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+        'glass': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+        'white': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+        'dkgrey': {
+			'ambientLightWeight': [0.0, 0.0, 0.0],
+			'diffuseLightWeight': [0.8, 0.8, 0.8],
+			'specularLightWeight': [0.6, 0.6, 0.6],
+			'shininess': 10.0,
+			'transparency': 1.0
+		},
+	},
+	'colorsMap': {
 		'yellow':   kh.RGBColorToColor( '#FFFF00'),
         'red':      kh.RGBColorToColor( '#FF0000'),
         'black':    [0.0, 0.0, 0.0, 1.0],
