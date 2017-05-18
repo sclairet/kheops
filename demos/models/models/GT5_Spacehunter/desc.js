75f33ce281be97a78fe9100b0d6c906cf4e5de40
sclairet <stephane.clairet@wakanda.io>
2017-05-18 18:31:01 +0200

    load materials file for models: step 1

--- demos/models/models/GT5_Spacehunter/desc.js
+++ demos/models/models/GT5_Spacehunter/desc.js
@@ -6,5 +6,6 @@ kh.models.desc = kh.models.desc || {};
 
 kh.models.desc.gt5_spacehunter = {
 	'name': 'gt5_spacehunter',
-	'src': './models/GT5_Spacehunter/GT5_Spacehunter.obj'
+	'src': './models/GT5_Spacehunter/GT5_Spacehunter.obj',
+	'mtl': './models/GT5_Spacehunter/GT5_Spacehunter.mtl'
 };
