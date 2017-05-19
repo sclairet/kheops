0403c3a9a9208b42a886218cb938f77eb3f3e0f4
sclairet <stephane.clairet@wakanda.io>
2017-05-19 15:47:49 +0200

    fix faces flattened indexes, fix textures coordinates, append models

---
+++ demos/models/models/SOC/desc.js
@@ -0,0 +1,15 @@
+
+var kh = kh || {};
+kh.models = kh.models || {};
+kh.models.desc = kh.models.desc || {};
+
+
+kh.models.desc.curtiss = {
+	'name': 'curtiss',
+	'src': './models/SOC/SOC.obj',
+	'mtl': './models/SOC/SOC.mtl',
+	'texturesMap': {
+		'SOCfusl.jpg': './models/SOC/SOCfusl.jpg',
+		'SOCwngs.JPG': './models/SOC/SOCwngs.JPG'
+	}
+};
