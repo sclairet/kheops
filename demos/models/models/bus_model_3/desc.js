064fbbb6ce6ae228f54a6bac0d1d1046b4180cd1
sclairet <stephane.clairet@wakanda.io>
2017-05-29 18:09:15 +0200

    append bus model

---
+++ demos/models/models/bus_model_3/desc.js
@@ -0,0 +1,11 @@
+
+var kh = kh || {};
+kh.models = kh.models || {};
+kh.models.desc = kh.models.desc || {};
+
+
+kh.models.desc.bus3 = {
+	'name': 'bus3',
+	'src': './models/bus_model_3/bus.obj',
+	'mtl': './models/bus_model_3/bus.mtl'
+};
