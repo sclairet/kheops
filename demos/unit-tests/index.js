cea64463fb508602277385ba419f822c27eb6cab
Stéphane Clairet <stephane.clairet@gmail.com>
2021-12-26 11:48:51 +0100

    update unit tests

--- demos/unit-tests/index.js
+++ demos/unit-tests/index.js
@@ -21,34 +21,32 @@ var loadScene = function loadScene(scene) {
     var right = scene.textureMgr.loadTexture( './textures/right.png', false);
 	var left = scene.textureMgr.loadTexture( './textures/left.png', false);
 
-
-	scene.xAxisRotationProgress = new kh.Progress( null, { 'infinite': true, 'start': 0, 'end': 359, 'step': 1});
-	scene.yAxisRotationProgress = new kh.Progress( null, { 'infinite': true, 'start': 0, 'end': 359, 'step': 1});
+	var objPage1 = new kh.Obj( scene, {});
 
 	var obj = kh.obj.triangle.create( scene, {'pos': [-12.0, 5.0, 0.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.triangle.create( scene, {'pos': [-9.0, 5.0, 0.0], 'color': [0.5, 0.5, 0.8, 1.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.triangle.create( scene, {'pos': [-6.0, 5.0, 0.0], 'colors': [1.0, 0.3, 0.3, 1.0, 0.3, 1.0, 0.3, 1.0, 0.3, 0.3, 1.0, 1.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.triangle.create( scene, {'pos': [-3.0, 5.0, 0.0], 'texture': front});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 
 	var obj = kh.obj.square.create( scene, {'pos': [-12.0, 2.0, 0.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.square.create( scene, {'pos': [-9.0, 2.0, 0.0], 'color': [0.3, 0.3, 0.6, 1.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.square.create( scene, {'pos': [-6.0, 2.0, 0.0], 'colors': [1.0, 0.3, 0.3, 1.0, 0.3, 1.0, 0.3, 1.0, 0.3, 0.3, 1.0, 1.0, 0.6, 0.6, 0.6, 1.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.square.create( scene, {'pos': [-3.0, 2.0, 0.0], 'texture': front});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 
 	var obj = kh.obj.disc.create( scene, {'pos': [-12.0, -1.0, 0.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.disc.create( scene, {'pos': [-9.0, -1.0, 0.0], 'color': [0.5, 0.5, 0.8, 1.0]});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.disc.create( scene, {'pos': [-6.0, -1.0, 0.0], 'texture': front});
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 
 	var doubleFaceSquares = [];
 	doubleFaceSquares.push( kh.obj.doubleFaceSquare.create( scene, {'pos': [-3.0, -1.0, 0.0]}));
@@ -62,11 +60,8 @@ var loadScene = function loadScene(scene) {
 		'back': back
 	};
 	doubleFaceSquares.push( kh.obj.doubleFaceSquare.create( scene, {'pos': [12.0, -1.0, 0.0], 'faceTextures': faceTextures}));
-	kh.installStaticScale( [0.8, 0.8, 0.8], doubleFaceSquares);
-	kh.installKineticRotation( [1.0, 0.0, 0.0], scene.xAxisRotationProgress, doubleFaceSquares);
-	kh.installKineticRotation( [0.0, 1.0, 0.0], scene.yAxisRotationProgress, doubleFaceSquares); 
 	for (var iter = 0 ; iter < doubleFaceSquares.length ; ++iter) {
-		scene.rootObject.addChildObject( doubleFaceSquares[iter]);
+		objPage1.addChildObject( doubleFaceSquares[iter]);
 	}
 
 	var cubes = [];
@@ -91,11 +86,9 @@ var loadScene = function loadScene(scene) {
 		'left': left
 	};
 	cubes.push( kh.obj.cube.create( scene, {'pos': [12.0, 5.0, 0.0], 'faceTextures': faceTextures}));
-	kh.installStaticScale( [0.7, 0.7, 0.7], cubes);
-	kh.installKineticRotation( [0.0, 1.0, 0.0], scene.yAxisRotationProgress, cubes);
-	kh.installKineticRotation( [1.0, 0.0, 0.0], scene.xAxisRotationProgress, cubes);
 	for (var iter = 0 ; iter < cubes.length ; ++iter) {
-		scene.rootObject.addChildObject( cubes[iter]);
+		objPage1.addChildObject( cubes[iter]);
+		scene.focusables.push(cubes[iter]);
 	}
 	
 	var cylinders = [];
@@ -108,21 +101,109 @@ var loadScene = function loadScene(scene) {
 		'bottom': bottom
 	};
 	cylinders.push( kh.obj.cylinder.create( scene, {'pos': [9.0, 2.0, 0.0], 'faceTextures': faceTextures} ));
-	kh.installStaticScale( [0.8, 0.8, 0.8], cylinders);
-	kh.installKineticRotation( [0.0, 1.0, 0.0], scene.yAxisRotationProgress, cylinders);
-	kh.installKineticRotation( [1.0, 0.0, 0.0], scene.xAxisRotationProgress, cylinders); 
 	for (var iter = 0 ; iter < cylinders.length ; ++iter) {
-		scene.rootObject.addChildObject( cylinders[iter]);
+		objPage1.addChildObject( cylinders[iter]);
+		scene.focusables.push(cylinders[iter]);
 	}
 
 	var obj = kh.obj.surface.create( scene, {'pos': [-12.0, -4.0, 0.0]} );
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.surface.create( scene, {'pos': [-9.0, -4.0, 0.0], 'color': [0.5, 0.5, 0.8, 1.0]} );
-	scene.rootObject.addChildObject( obj);
+	objPage1.addChildObject( obj);
 	var obj = kh.obj.surface.create( scene, {'pos': [-6.0, -4.0, 0.0], 'texture': front} );
-	scene.rootObject.addChildObject( obj);		
+	objPage1.addChildObject( obj);
+
+	kh.installStaticScale( [0.9, 0.9, 0.9], objPage1.children());
+
+	scene.rootObject.addChildObject(objPage1);
+
+
+	var objPage2 = new kh.Obj( scene, {'visible': false});
+
+	var faceTextures = {
+		'front': front,
+		'back': back,
+		'top': top,
+		'bottom': bottom,
+		'right': right,
+		'left': left
+	};
+
+
+	var vertexDesc = {
+		'width': 2.0,
+		'height': 1.0,
+		'depth': 0.6,
+	}
+
+	var pos = [-10.0, -7.0, -4.0, -1.0, 2.0]
+	for (var iter = 0 ; iter < 5 ; ++iter) {
+		var obj = kh.obj.cube.create( scene, {
+	    	'pos': [pos[iter], 5.0, 0.0],
+	    	'faceTextures': faceTextures,
+	    	'vertices': kh.primitive.cube.createVertexPosArrayFromDesc(vertexDesc),
+	    	'normals': kh.primitive.cube.createVertexNormalsArrayFromDesc(vertexDesc),
+	    	'textureCoords': kh.primitive.cube.createVertexTextureCoordArrayFromDesc(vertexDesc, iter)
+	    } );
+		scene.focusables.push(obj);
+	    objPage2.addChildObject( obj);
+	}
+
+	var vertexDesc = {
+		'coords': [
+			// top left front
+	/*0*/	[0.0, 1.0, 2.0],
+			// bottom left front
+	/*1*/	[0.0, -1.0, 1.0],
+			// bottom right front
+	/*2*/	[1.0, -1.0, 1.0],
+			// top right front
+	/*3*/	[2.0, 1.0, 2.0],
+			// top left back
+	/*4*/	[0.0, 1.0, 0.0],
+			// bottom left back
+	/*5*/	[0.0, -1.0, 0.0],
+			// bottom right back
+	/*6*/	[1.0, -1.0, 0.0],
+			// top right back
+	/*7*/	[2.0, 1.0, 0.0],
+		]
+	};
+
+	var pos = [-10.0, -6.0, -2.0, 2.0, 6.0]
+	for (var iter = 0 ; iter < 5 ; ++iter) {
+		var obj = kh.obj.cube.create( scene, {
+	    	'pos': [pos[iter], 1.0, 0.0],
+	    	'faceTextures': faceTextures,
+	    	'vertices': kh.primitive.cube.createVertexPosArrayFromDesc(vertexDesc),
+	    	'normals': kh.primitive.cube.createVertexNormalsArrayFromDesc(vertexDesc),
+	    	'textureCoords': kh.primitive.cube.createVertexTextureCoordArrayFromDesc(vertexDesc, iter)
+	    } );
+		scene.focusables.push(obj);
+	    objPage2.addChildObject( obj);
+	}
+
+	scene.rootObject.addChildObject(objPage2);
+
+	var objPage3 = new kh.Obj( scene, {'visible': false});
+
+	var obj = kh.obj.cube.create( scene, {'pos': [-3.0, 0.0, 10.0], 'color': [1.0, 1.0, 1.0, 1.0], 'faceTextures': faceTextures});
+    kh.installKineticRotation( [0.0, 1.0, 0.0], new kh.Progress( scene.scheduler, { 'infinite': true, 'start': 0, 'end': 359, 'step': 0.6}), [obj]);
+    kh.installKineticRotation( [0.0, 0.0, 1.0], new kh.Progress( scene.scheduler, { 'infinite': true, 'start': 0, 'end': 359, 'step': 0.5}), [obj]);
+    kh.installKineticRotation( [1.0, 0.0, 0.0], new kh.Progress( scene.scheduler, { 'infinite': true, 'start': 0, 'end': 359, 'step': 0.4}), [obj]);    
+    objPage3.addChildObject( obj);
+
+    var obj = kh.obj.schizoidCube.create( scene, {'pos': [3.0, 0.0, 10.0], 'faceTextures': faceTextures}, scene);
+    objPage3.addChildObject( obj);
+    scene.focusables.push(obj);
+    scene.rootObject.addChildObject(objPage3);
 
 	kh.installStaticScale( [1.2, 1.2, 1.2], [scene.rootObject]);
+
+	scene.hideables.push(objPage1);
+	scene.hideables.push(objPage2);
+	scene.hideables.push(objPage3);
+	scene.hideables.begin();
 };
 
 
@@ -139,6 +220,7 @@ function loadEventHandler() {
     gl.enable( gl.CULL_FACE);
 
     var props = {};
+    props.withKeydownHandler = true;
     props.camera = {};
     props.camera.bounds = { x: 0, y: 0, width: document.documentElement.clientWidth, height: document.documentElement.clientHeight};
     props.camera.pos = [0.0, 0.0, -25.0];
@@ -166,22 +248,7 @@ var unloadEventHandler = function unloadEventHandler() {
 
 var keydownEventHandler = function keydownEventHandler(keydownEvent) {
 
-	if (keydownEvent.key == ' ') {
-		this.scheduler.enabled = (this.scheduler.enabled) ? false : true;
-	}
-	else if (keydownEvent.key == '6') {
-		kh.gScene.yAxisRotationProgress.mCurrent+=kh.gScene.yAxisRotationProgress.mStep;
-	}
-	else if (keydownEvent.key == '4') {
-		kh.gScene.yAxisRotationProgress.mCurrent-=kh.gScene.yAxisRotationProgress.mStep;
-	}
-	else if (keydownEvent.key == '8') {
-		kh.gScene.xAxisRotationProgress.mCurrent-=kh.gScene.xAxisRotationProgress.mStep;
-	}
-	else if (keydownEvent.key == '2') {
-		kh.gScene.xAxisRotationProgress.mCurrent+=kh.gScene.xAxisRotationProgress.mStep;
-	}
-	else {
+	{
 		kh.gScene.onKeydown(keydownEvent);
 	}
 };
