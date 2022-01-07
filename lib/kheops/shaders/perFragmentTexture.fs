02fe40d61dd61da3935ca7634f9bebe264f87c43
Stéphane Clairet <stephane.clairet@gmail.com>
2022-01-07 11:33:21 +0100

    point light support: support ten point lights, update shaders

--- lib/kheops/shaders/perFragmentTexture.fs
+++ lib/kheops/shaders/perFragmentTexture.fs
@@ -1,13 +1,19 @@
 precision mediump float;
 
+
+// ambient light
 uniform vec3 uAmbientColor;
 
+// directional light
 // reverse lighting direction
 uniform vec3 uLightingDirection;
 uniform vec3 uDirectionalColor;
 
-uniform vec3 uPointLightColor_0;
-uniform vec3 uPointLightDirection_0;
+// point lights
+#define pointLightsCount 10
+uniform int uPointLightsEnabled[pointLightsCount];
+uniform vec3 uPointLightsColor[pointLightsCount];
+uniform float uPointLightsDistance[pointLightsCount];
 
 uniform vec3 uAmbientReflection;
 uniform vec3 uDiffuseReflection;
@@ -22,13 +28,13 @@ uniform mat4 uPMatrix;
 varying vec3 vTransformedNormal;
 varying vec4 vPosition;
 varying vec2 vTextureCoord;
-varying vec3 vPointLightDirection_0;
-varying float vPointLightDistance_0;
+varying vec3 vCurrentPointLightsDirection[pointLightsCount];
+varying float vCurrentPointLightsDistance[pointLightsCount];
 
 void main(void) {
 
 	vec3 surfaceNormal = normalize( vTransformedNormal);
-	vec3 pointLightDirection_0 = normalize(vPointLightDirection_0);
+	vec3 directionalLightDirection = normalize(uLightingDirection);
 
 	vec3 viewDirection = vec3( 0.0, 0.0, 1.0);
 	
@@ -38,39 +44,40 @@ void main(void) {
 	lightWeighting += (uAmbientColor * uAmbientReflection);
 	
 	// directional lightweight
-	float directionalDotProduct = min( dot( surfaceNormal, uLightingDirection), 1.0);
+	float directionalDotProduct = dot( surfaceNormal, directionalLightDirection);
 	if (directionalDotProduct > 0.0) {
 		lightWeighting += (uDirectionalColor * uDiffuseReflection * directionalDotProduct);
-	}
 
-	// point light
-	float pointDotProduct = min(dot( surfaceNormal, vPointLightDirection_0), 1.0);
-	if (pointDotProduct > 0.0) {
-		lightWeighting += (uPointLightColor_0 * uDiffuseReflection * pointDotProduct);
-	}
-
-	// specular lightweight
-	if (directionalDotProduct > 0.0) {
-		vec3 reflectionDirection = reflect( -uLightingDirection, surfaceNormal);
-		float dotProduct = min(dot( reflectionDirection, viewDirection), 1.0);
-		if (dotProduct > 0.0) {
-			dotProduct = pow( dotProduct, uShininess);
-			lightWeighting += (uDirectionalColor * uSpecularReflection * dotProduct);
+		// specular lightweight
+		if (length(uSpecularReflection) > 0.0) {
+			vec3 reflectionDirection = reflect( -directionalLightDirection, surfaceNormal);
+			float dotProduct = dot( reflectionDirection, viewDirection);
+			if (dotProduct > 0.0) {
+				dotProduct = pow( dotProduct, uShininess);
+				lightWeighting += (uDirectionalColor * uSpecularReflection * dotProduct);
+			}
 		}
 	}
 
-	if (pointDotProduct > 0.0) {
-		vec3 reflectionDirection = reflect( -vPointLightDirection_0, surfaceNormal);
-		float dotProduct = min(dot( reflectionDirection, viewDirection), 1.0);
-		if (dotProduct > 0.0) {
-			dotProduct = pow( dotProduct, uShininess);
-			lightWeighting += (uPointLightColor_0 * uSpecularReflection * dotProduct * 0.01);
-		}
-	}
+	// point lights
+	for (int i = 0; i < pointLightsCount; ++i) {
+		if (length(vCurrentPointLightsDirection[i]) > 0.0) {
+			vec3 normalizedPointLightDirection = normalize(vCurrentPointLightsDirection[i]);
+			float dotProduct = dot( surfaceNormal, normalizedPointLightDirection);
+			if (dotProduct > 0.0) {
+				lightWeighting += (uPointLightsColor[i] * uDiffuseReflection * dotProduct);
 
-	//vec3 lightWeighting =		(uAmbientColor * uAmbientReflection)
-	//						+	(uDirectionalColor * uDiffuseReflection * directionalLightWeight)
-	//						+	(uPointLightColor_0 * uDiffuseReflection * pointLightWeight0);
+			}
+			if (length(uSpecularReflection) > 0.0) {
+				vec3 reflectionDirection = reflect( -normalizedPointLightDirection, surfaceNormal);
+				dotProduct = dot( reflectionDirection, viewDirection);
+				if (dotProduct > 0.0) {
+					dotProduct = pow( dotProduct, uShininess);
+					lightWeighting += (uPointLightsColor[i] * uSpecularReflection * dotProduct);
+				}
+			}
+		}
+    }
 
 	vec4 lColor = texture2D( uSampler, vTextureCoord);
 
--- lib/kheops/shaders/perFragmentTexture.vs
+++ lib/kheops/shaders/perFragmentTexture.vs
@@ -5,20 +5,35 @@ attribute vec2 aTextureCoord;
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 uniform mat3 uNMatrix;
-uniform vec3 uPointLightPosition_0;
+
+// point lights
+#define pointLightsCount 10
+uniform int uPointLightsEnabled[pointLightsCount];
+uniform vec3 uPointLightsPosition[pointLightsCount];
 
 varying vec3 vTransformedNormal;
 varying vec4 vPosition;
 varying vec2 vTextureCoord;
-varying vec3 vPointLightDirection_0;
-varying float vPointLightDistance_0;
+varying vec3 vCurrentPointLightsDirection[pointLightsCount];
+varying float vCurrentPointLightsDistance[pointLightsCount];
 
 void main(void) {
 	vPosition = uMVMatrix * vec4(aVertexPosition, 1.0);
-	vec4 pointLightPosition_0 = vec4(uPointLightPosition_0, 1.0);
-	vPointLightDistance_0 = distance(vPosition, pointLightPosition_0) ;
-	vPointLightDirection_0 = (pointLightPosition_0 - vPosition).xyz;
+
 	gl_Position = uPMatrix * vPosition;
 	vTransformedNormal = uNMatrix * aVertexNormal;
 	vTextureCoord = aTextureCoord;
+
+	// point lights
+	for (int i = 0; i < pointLightsCount; ++i) {
+		if (uPointLightsEnabled[i] == 1) {
+			vec4 pointLightPos = vec4(uPointLightsPosition[i], 1.0);
+			vCurrentPointLightsDistance[i] = distance(vPosition, pointLightPos) ;
+			vCurrentPointLightsDirection[i] = (pointLightPos - vPosition).xyz;
+		}
+		else {
+			vCurrentPointLightsDirection[i] = vec3(0.0, 0.0, 0.0);
+		}
+    }
+
 }
